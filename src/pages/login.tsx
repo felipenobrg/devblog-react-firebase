a60ea1224d2fb0e14afc0a8231d4603187c795a9
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-04-21 17:30:31 -0300

    adding google authentication

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,19 @@
+import { auth, provider } from "../config/firebase"
+import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from "react-router-dom"
+
 export const Login = () => {
+    const navigate = useNavigate()
+
+    const signInWithGoogle = async () => {
+      const result = await signInWithPopup(auth, provider)
+      
+    }
 
     return ( 
-    <div> Login Page</div>
+    <>
+    <div><p>Sign In With Google To Continue</p></div>
+    <button onClick={signInWithGoogle}>Sign in With Google</button>
+    </>
     )
 }
