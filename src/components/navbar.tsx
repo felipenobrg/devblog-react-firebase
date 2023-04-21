dd02057d1c99014a1d93028b52ec1012bceb4965
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-04-21 17:31:08 -0300

    adding google authentication

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom"
+import { auth } from "../config/firebase"
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 export const Navbar = () => {
+    const [user] = useAuthState(auth)
 
     return (
      <>
-     <Link to="/">Home</Link>
-     <Link to="/login">Login</Link>
+     <Link to="/"> Home </Link>
+     <Link to="/login"> Login </Link>
+
+     <div>
+        <p>{user?.displayName}</p>
+        <img src={user?.photoURL || ""}  width="100" height="100" />
+     </div>
      </>
     )
 }
